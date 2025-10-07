8a82509ade38cd4fabdadba1af20af2657f51c77
zhuwenwei <543053433@qq.com>
2025-10-07 17:02:29 +0800

    reinitialized files

---
+++ test/test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by zhuwenwei on 2021/1/12.
+//  Copyright © 2021 zhuwenwei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
