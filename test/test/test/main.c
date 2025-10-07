35c07f7b6e7515fb1915dd1c3da08cf8a4c3296b
zhuwenwei <543053433@qq.com>
2025-10-07 17:04:15 +0800

    测试
    测试

--- test/test/test/main.c
+++ test/test/test/main.c
@@ -8,8 +8,71 @@
 
 #include <stdio.h>
 
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float R1,R2,U,I1,I2;
+
+//    printf("Input three numbers R1,R2,U:\n");
+//    scanf("%f,%f,%f",&R1,&R2,&U);
+//    printf("R1=%f,R2=%f,U=%f\n",R1,R2,U);
+//    I1=U/(R1+R2);
+//    I2=U/((R1*R2)/(R1+R2));
+//    printf("I1=%f,I2=%f\n",I1,I2);
+//    int a = 3;
+//    printf("%d\n", a);
+    
+
+//    for (int i = 0; i < width; i++){
+//        for (int j = 0; j < height; j++){
+//            printf("(%d, %d),", i, j);
+//        }
+//        printf("\n");
+//    }
+    
+    
+    int width = 9;//宽度
+    int height = 9;//高度
+    int index;//序号
+    for (int i = width-1; i >= 0; i--){
+        for (int j = height-1; j >= 0; j--){
+            index = i*width+j;
+            if (i != 0 && i != height-1 && j != 0 && j != width-1 && i%2 == 1 && j%2 == 1){
+                printf("%d,", index);
+                //设置颜色
+                
+            }
+        }
+        printf("\n");
+    }
+    
+    
+    
+//    int width = 5;//宽度
+//    int height = 5;//高度
+//    int index;//序号
+//    for (int i = width-1; i >= 0; i--){
+//        for (int j = height-1; j >= 0; j--){
+//            index = i*width+j;
+//            if (i != 0 && i != height-1 && j != 0 && j != width-1 && i%2 == 1 && j%2 == 1){
+//                //设置颜色
+//
+//            }
+//        }
+//    }
+    
+    
+    
+//    scanf("%f",&R1);
+//    scanf("%f",&R2);
+//    scanf("%f",&U);
+//    scanf("%f,%f,%f",&R1,&R2,&U);
+//    printf("R1=%f,R2=%f,U=%f",R1,R2,U);
+//    I1=U/(R1+R2);
+//    I2=U/((R1*R2)/(R1+R2));
+//    printf("I1=%f,I2=%f",I1,I2);
+//    getch();
+    
     return 0;
 }
+
+
